475cd1f710b816f9db60c473972a94926ac5be8b
Panagiotis Meremetis <mere.p.w_@hotmail.com>
2024-10-06 20:45:19 +0300

    add more shit

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,14 +1,44 @@
 import { LowerCasePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [LowerCasePipe],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  // it is use full when you have certain properties to be added to an element.
+  // recommended by angular
+  host: {
+    class: 'control',
+    // ,'(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
   title = input.required<string>();
 
+  private el = inject(ElementRef);
+
+  // adding ptoperty old way
+  // @HostBinding('class') className = 'control';
+
+  // adding event listener
+  // @HostListener('click') onClick() {
+  //   console.log('clicked');
+  // }
+
+  @HostListener('input', ['$event']) onInput(event: Event) {
+    console.log('input', event);
+    console.log("el", this.el);
+
+  }
 }
