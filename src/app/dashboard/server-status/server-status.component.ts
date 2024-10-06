f84b3887f2659c08e45f7dfb0b89a0900443d652
mere <mere.p.w_@hotmail.com>
2024-10-06 21:32:41 +0300

    add shit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown';
+
+  constructor() {
+    this.currentStatus = 'offline';
+  }
+
+  // the best practice is to not do things in constructor other than basic property initialization.
+  // an other thing to keep in mind is that component inputs are done initialized after the constructor, hence they are available into onInit but not on constructor.
+  ngOnInit() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
 }
